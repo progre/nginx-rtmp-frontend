c5a6c92ebe408ebc7e710a6135ead28abfe3fcce
progre <djyayutto@gmail.com>
2015-11-25 00:14:37 +0900

    Release 0.0.32

---
+++ scripts/packosx.bat
@@ -0,0 +1,3 @@
+electron-packager dest/ %APP_NAME% --platform=darwin --arch=x64 --version=%ELECTRON_VER% --icon=src/res/icon.icns
+zip -qry %APP_NAME%-osx.zip %APP_NAME%-darwin-x64 -x '*.exe'
+rm -rf %APP_NAME%-darwin-x64
--- scripts/packwin.bat
+++ scripts/packwin.bat
@@ -1,4 +1,5 @@
 electron-packager dest/ %APP_NAME% --platform=win32 --arch=ia32 --version=%ELECTRON_VER% --icon=src/res/icon_256.ico
 grunt create-windows-installer
+copy installer32\Setup.exe %APP_NAME%-win-installer.exe
 zip -qry %APP_NAME%-win.zip %APP_NAME%-win32-ia32 -x '*.osx'
 rm -rf %APP_NAME%-win32-ia32
