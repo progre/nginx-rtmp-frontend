0aecbaf437575b4e9f8552d4d533fe9eadd9706c
progre <djyayutto@gmail.com>
2015-11-24 22:58:31 +0900

    Release 0.0.25

--- scripts/packwin.bat
+++ scripts/packwin.bat
@@ -1,4 +1,7 @@
+where electron-packager
 electron-packager dest/ %APP_NAME% --platform=win32 --arch=ia32 --version=%ELECTRON_VER% --icon=src/res/icon_256.ico
+where grunt
 grunt create-windows-installer
+where zip
 zip -qry %APP_NAME%-win.zip %APP_NAME%-win32-ia32 -x '*.osx'
 rm -rf %APP_NAME%-win32-ia32
