36e27b158372c0e935e9faf87fefedfe23f16e8f
progre <djyayutto@gmail.com>
2015-11-26 06:07:24 +0900

    Revert "WIP"
    This reverts commit 635b6da3d3f977ed4e8e4e33c3ba98dce80b8334.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,8 @@
 'use strict';
 try { require('source-map-support').install(); } catch (e) { /* empty */ }
 require('crash-reporter').start();
-const app = require('app');
-const autoUpdater = require('auto-updater');
-import * as https from 'https';
-import promisify from 'native-promisify';
+import * as app from 'app';
 import * as log4js from 'log4js';
-const logger = log4js.getLogger();
-const GitHubApi = require('github');
 import Application from './application';
 
 log4js.configure({
@@ -18,70 +13,27 @@ log4js.configure({
     }]
 });
 
-function main() {
-    let squirrelCommand = process.argv[1];
+async function main() {
+    var squirrelCommand = process.argv[1];
     switch (squirrelCommand) {
         case '--squirrel-install':
         case '--squirrel-updated':
+            app.quit();
             return;
         case '--squirrel-uninstall':
+            app.quit();
             return;
         case '--squirrel-obsolete':
+            app.quit();
             return;
-        default:
     }
 
-    checkUpdate().catch(e => {
+    try {
+        await Application.new();
+    } catch (e) {
         log4js.getLogger().fatal(e);
         app.quit();
-    });
-    Application.new().catch(e => {
-        log4js.getLogger().fatal(e);
-        app.quit();
-    });
-}
-
-async function checkUpdate() {
-    let github = new GitHubApi({
-        version: '3.0.0',
-        debug: false
-    });
-    let json = <any[]>await promisify(github.releases.listReleases)({
-        owner: 'progre',
-        repo: 'nginx-rtmp-frontend'
-    });
-    let latest = json.filter(x => !x.draft && !x.prerelease)
-        .filter(x => (<any[]>x.assets)
-            .filter((x: any) => x.name === 'update.json')
-            .length > 0)[0];
-    // if (latest.tag_name === app.getVersion()) {
-    //     setTimeout(checkUpdate, 24 * 60 * 60 * 1000);
-    //     return;
-    // }
-    let url = latest.assets.filter((x: any) => x.name === 'update.json')[0].browser_download_url;
-    logger.info(url);
-    autoUpdater.setFeedURL(url);
-    autoUpdater.on('update-not-available', () => {
-        logger.info('update-not-available');
-    });
-    autoUpdater.on('update-available', () => {
-        logger.info('update-available');
-    });
-    autoUpdater.on('update-downloaded', () => {
-        logger.info('update-downloaded');
-        app.on('quit', () => {
-            logger.info('quit!');
-            (<any>autoUpdater).quitAndInstall();
-        });
-    });
-    autoUpdater.checkForUpdates();
-    // const releaseURL = 'https://api.github.com/repos/progre/nginx-rtmp-frontend/releases';
-    // https.get(releaseURL, res => {
-    //     logger.info('asdf', res.statusCode);
-    //     res.setEncoding('utf-8');
-    //     let json = <any[]>JSON.parse(res.read());
-    //     logger.info('', json);
-    // });
+    }
 }
 
 main();
