9eda90228a4ab681b8af473984dcb03730d69ab2
progre <djyayutto@gmail.com>
2015-11-25 12:39:50 +0900

    Update

--- scripts/packwin.bat
+++ scripts/packwin.bat
@@ -1,11 +1,6 @@
-echo 1
 call electron-packager dest/ %APP_NAME% --platform=win32 --arch=ia32 --version=%ELECTRON_VER% --icon=src/res/icon_256.ico
-echo 2
 call grunt create-windows-installer
-echo 3
 copy installer32\Setup.exe %APP_NAME%-win-installer.exe
-echo 4
-call zip -qry %APP_NAME%-win.zip %APP_NAME%-win32-ia32 -x '*.osx'
-echo 5
+call 7za.exe a %APP_NAME%-win.zip %APP_NAME%-win32-ia32
 rm -rf %APP_NAME%-win32-ia32
-echo 6
+
