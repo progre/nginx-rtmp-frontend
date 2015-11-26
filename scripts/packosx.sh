694f1af23deec26bad47602056cd73e11673f4f6
progre <djyayutto@gmail.com>
2015-11-26 20:32:52 +0900

    Fix build

--- scripts/packosx.sh
+++
@@ -1,3 +0,0 @@
-electron-packager dest/ $APP_NAME --platform=darwin --arch=x64 --version=$ELECTRON_VER --icon=src/res/icon.icns
-zip -qry $APP_NAME-osx.zip $APP_NAME-darwin-x64 -x '*.exe'
-rm -rf $APP_NAME-darwin-x64
--- scripts/packwin.bat
+++
@@ -1,3 +0,0 @@
-call electron-packager dest/ %APP_NAME% --platform=win32 --arch=ia32 --version=%ELECTRON_VER% --icon=src/res/icon_256.ico --asar=true
-call 7za.exe a %APP_NAME%-win.zip %APP_NAME%-win32-ia32
-rm -rf %APP_NAME%-win32-ia32
