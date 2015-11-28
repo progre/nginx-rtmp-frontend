913420339bb7c2898d715678f8f14c2185fcc18a
progre <djyayutto@gmail.com>
2015-11-28 12:42:20 +0900

    Fix package

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 'use strict';
-const mkdir = require('native-promisify')(require('fs').mkdir);
-const exec = require('native-promisify')(require('child_process').exec);
+const promisify = require('native-promisify');
+const mkdir = promisify(require('fs').mkdir);
+const exec = promisify(require('child_process').exec);
+const sep = require('path').sep;
 const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
+const electronPackager = promisify(require('electron-packager'));
 
 require('./gulp/copy')();
 require('./gulp/jade')();
@@ -41,13 +44,50 @@ gulp.task('watch', () => {
     gulp.watch('src/test/**/*.ts', ['test']);
 });
 
-gulp.task('copy-to-dest', () => {
-    return mkdir('dest')
-        .then(() => exec('cp -r lib/ dest/lib')).then(printStdout)
-        .then(() => exec('cp LICENSE dest/')).then(printStdout)
-        .then(() => exec('cp package.json dest/')).then(printStdout)
-        .then(() => exec('cp README*.md dest/')).then(printStdout)
-        .then(() => exec('npm install --production', { cwd: 'dest' })).then(printStdout);
+gulp.task('package', () => {
+    const APP_NAME = require('./package.json').name;
+    const ELECTRON_VER = '0.35.1';
+    return mkdir('tmp').catch(errorHandler)
+        .then(() => mkdir('tmp/dest')).catch(errorHandler)
+        .then(() => exec('cp -r lib/ tmp/dest/lib')).then(printStdout)
+        .then(() => exec('cp LICENSE tmp/dest/')).then(printStdout)
+        .then(() => exec('cp package.json tmp/dest/')).then(printStdout)
+        .then(() => exec('cp README*.md tmp/dest/')).then(printStdout)
+        .then(() => exec('npm install --production', { cwd: 'tmp/dest' })).then(printStdout)
+        .then(() => execPackageAndZip('tmp', 'dest', 'darwin', 'x64', 'src/res/icon.icns'))
+        .then(() => execPackageAndZip('tmp', 'dest', 'win32', 'ia32', 'src/res/icon_256.ico'))
+        .then(() => execPackageAndZip('tmp', 'dest', 'linux', 'x64', null));
+
+    function execPackageAndZip(cwd, path, platform, arch, icon) {
+        let os = (() => {
+            switch (platform) {
+                case 'darwin': return 'osx';
+                case 'win32': return 'win';
+                case 'linux': return 'linux';
+            }
+        })();
+        let zipPath = `tmp/${APP_NAME}-${platform}-${arch}`;
+        return electronPackager(
+            {
+                dir: `${cwd}/${path}`,
+                name: APP_NAME,
+                platform,
+                arch,
+                version: ELECTRON_VER,
+                icon,
+                asar: true,
+                out: cwd
+            })
+            .then(printStdout)
+            .then(() => exec(`zip -qry ../../${APP_NAME}-${os}.zip .`, { cwd: zipPath }))
+            .then(printStdout);
+    }
+
+    function errorHandler(e) {
+        if (e.code !== 'EEXIST') {
+            throw e;
+        }
+    }
 
     function printStdout(stdout) {
         if (stdout.length > 0) {
