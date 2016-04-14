f9c4669c83d51ffe582f4567ec512364d706e418
progre <djyayutto@gmail.com>
2016-04-14 13:00:33 +0900

    WIP

--- src/public/js/component/servicesettings.tsx
+++ src/public/js/component/servicesettings.tsx
@@ -1,248 +1,125 @@
 import * as React from "react";
 
 export default function ServiceSettings() {
-    return     <fieldset>
-      <legend className="i18n-service-settings"></legend>
-      <div className="row">
-        <Menu/>
-        <Contents/>
-      </div>
+    let services = [
+        {
+            name: "twitch",
+            icon: "http://www.twitch.tv/favicon.ico",
+            url: "http://www.twitch.tv/",
+            label: "Twitch"
+        },
+        {
+            name: "peercaststation",
+            icon: "http://127.0.0.1:7144/html/favicon.ico",
+            url: "http://www.pecastation.org/",
+            label: "PeerCastStation"
+        },
+        // {
+        //     name: "cavetube",
+        //     icon: null,
+        //     url: null,
+        //     label: "CaveTube"
+        // },
+        {
+            name: "livecodingtv",
+            icon: "https://www.livecoding.tv/favicon.ico",
+            url: "https://www.livecoding.tv/",
+            label: "Livecoding.tv"
+        },
+        {
+            name: "niconico",
+            icon: "http://www.nicovideo.jp/favicon.ico",
+            url: "http://live.nicovideo.jp/",
+            label: "niconico"
+        },
+        {
+            name: "other",
+            icon: null,
+            label: "Other"
+        }
+    ];
+    return <fieldset>
+        <legend className="i18n-service-settings"></legend>
+        <div className="row">
+            <Menu services={services}/>
+            <ContentsWrapper/>
+        </div>
     </fieldset>;
 }
 
-// button.btn.btn-secondary.text-left#cavetube-button
-//   span.pull-left CaveTube
-//   span.pull-right
-//     i.fa.fa-check#cavetube-check
-
-function Menu() {
-    return         <div className="col-sm-4 btn-group-vertical">
-          <button id="twitch-button" className="btn btn-secondary">
-            <span style={{width: 16, height: 16}} className="pull-left">
-            <img src="http://www.twitch.tv/favicon.ico" width="16" height="16" onerror="this.style.display = 'none';"/>
-            </span>
-            <span style={{marginLeft: "0.5em"}} className="pull-left">Twitch</span>
-            <span className="pull-right"><i id="twitch-check" className="fa fa-check"/>          </span>
-          </button>
-          <button id="peercaststation-button" className="btn btn-secondary text-left">
-          <span style={{width: 16, height: 16}} className="pull-left">
-          <img src="http://127.0.0.1:7144/html/favicon.ico" width="16" height="16" onerror="this.style.display = 'none';"/>
-          </span>
-          <span style={{marginLeft: "0.5em"}} className="pull-left">PeerCastStation</span>
-          <span className="pull-right"><i id="peercaststation-check" className="fa fa-check"/></span>
-          </button>
-          <button id="livecodingtv-button" className="btn btn-secondary text-left">
-          <span style={{width: 16, height: 16}} className="pull-left">
-          <img src="https://www.livecoding.tv/favicon.ico" width="16" height="16" onerror="this.style.display = 'none';"/>
-          </span>
-          <span style={{marginLeft: "0.5em"}} className="pull-left">Livecoding.tv</span>
-          <span className="pull-right"><i id="livecodingtv-check" className="fa fa-check"/></span>
-          </button>
-          <button id="niconico-button" className="btn btn-secondary text-left">
-          <span style={{width: 16, height: 16}} className="pull-left">
-          <img src="http://www.nicovideo.jp/favicon.ico" width="16" height="16" onerror="this.style.display = 'none';"/>
-          </span>
-          <span style={{marginLeft: "0.5em"}} className="pull-left">niconico</span>
-          <span className="pull-right"><i id="niconico-check" className="fa fa-check"/>
-          </span>
-          </button>
-          <button id="other-button" className="btn btn-secondary text-left">
-          <span className="pull-left"><img width="16" height="16"/></span>
-          <span style={{marginLeft: "0.5em"}} className="pull-left">Other</span>
-          <span className="pull-right"><i id="other-check" className="fa fa-check"/></span>
-          </button>
-        </div>;
+function Menu(props: { services: { name: string; icon: string; label: string; }[] }) {
+    return <div className="col-sm-4 btn-group-vertical">
+        {
+            props.services.map(service =>
+                <MenuItem
+                    name={service.name}
+                    icon={service.icon}
+                    label={service.label}/>)
+        }
+    </div>;
 }
 
-function TwitchContents() {
-    return <div style={{display: "none"}} id="twitch-option" className="col-sm-8">
-          <div className="row">
+function MenuItem(
+    props: { name: string; icon: string; label: string; }
+) {
+    return <button id={`${props.name}-button`} className="btn btn-secondary text-left">
+        <span style={{ width: 16, height: 16 }} className="pull-left">
+            <img src={props.icon} width="16" height="16" onerror="this.style.display = 'none';"/>
+        </span>
+        <span style={{ marginLeft: "0.5em" }} className="pull-left">Other</span>
+        <span className="pull-right"><i id={`${props.name}-check`} className="fa fa-check"/></span>
+    </button>;
+}
+
+function ContentsWrapper(
+    props: { services: { name: string; url: string; }[] }
+) {
+    return <div>
+        {
+            props.services.map(service =>
+                <Contents name={service.name} url={service.url}/>)
+        }
+    </div>;
+}
+
+function Contents(props: { name: string, url: string }) {
+    return <div style={{ display: "none" }} id={`${props.name}-option`} className="col-sm-8">
+        <div className="row">
             <div className="col-sm-push-3 col-sm-9">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" id="twitch-enabled"/><span className="i18n-enable"></span>
+                <div className="checkbox">
+                    <label>
+                        <input type="checkbox" id={`${props.name}-enabled`}/>
+                        <span className="i18n-enable"/>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-sm-3 text-right">
+                <label for={`${props.name}-fms`} className="form-control-static">
+                    FMS URL:
                 </label>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="twitch-fms" className="form-control-static">FMS URL:</label>
             </div>
             <div className="col-sm-9">
-              <select style={{height: 40}} id="twitch-fms" className="form-control"></select>
+                {
+                    props.name === "twitch"
+                        ? <select style={{ height: 40 }} id="twitch-fms" className="form-control"/>
+                        : <input style={{ width: "100%" }} type="text" id={`${props.name}-fms`} className="form-control"/>
+                }
             </div>
-          </div>
-          <div className="row">
+        </div>
+        <div className="row">
             <div className="col-sm-3 text-right">
-              <label for="twitch-key" className="form-control-static i18n-stream-key"></label>
+                <label for={`${props.name}-key`} className="form-control-static i18n-stream-key"/>
             </div>
             <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="twitch-key" className="form-control"/>
+                <input style={{ width: "100%" }} type="text" id={`${props.name}-key`} className="form-control"/>
             </div>
-          </div>
-          <div style={{marginTop: "1em"}} className="row">
+        </div>
+        <div style={{ marginTop: "1em" }} className="row">
             <div className="col-sm-12 text-right">
-            <a href="http://www.twitch.tv/" target="_blank" className="selectable">http://www.twitch.tv/</a>
+                <a href={props.url} target="_blank" className="selectable">{props.url}</a>
             </div>
-          </div>
-        </div>;
-}
-
-function PeerCastStationContents() {
-    return <div style={{display: "none"}} id="peercaststation-option" className="col-sm-8">
-          <div className="row">
-            <div className="col-sm-push-3 col-sm-9">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" id="peercaststation-enabled"/><span className="i18n-enable"></span>
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="peercaststation-fms" className="form-control-static">FMS URL:</label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="peercaststation-fms" className="form-control"/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-push-3 col-sm-9 i18n-notification-for-peercaststation"></div>
-          </div>
-          <div style={{marginTop: "1em"}} className="row">
-            <div className="col-sm-12 text-right">
-            <a href="http://www.pecastation.org/" target="_blank" className="selectable">http://www.pecastation.org/</a>
-            </div>
-          </div>
-        </div>;
-}
-
-// .col-sm-8(style='display: none;')#cavetube-option
-//   .row
-//     .col-sm-push-3.col-sm-9
-//       .checkbox
-//         label
-//           input(type='checkbox')#cavetube-enabled
-//           span.i18n-enable
-//   .row
-//     .col-sm-3.text-right
-//       label.form-control-static(for='cavetube-fms') FMS URL:
-//     .col-sm-9
-//       input.form-control(style='width: 100%', type='text')#cavetube-fms
-//   .row
-//     .col-sm-3.text-right
-//       label.form-control-static.i18n-stream-key(for='cavetube-key')
-//     .col-sm-9
-//       input.form-control(style='width: 100%', type='text')#cavetube-key
-
-function LivecodingTvContents() {
-       return <div style={{display: "none"}} id="livecodingtv-option" className="col-sm-8">
-          <div className="row">
-            <div className="col-sm-push-3 col-sm-9">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" id="livecodingtv-enabled"/><span className="i18n-enable"></span>
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="livecodingtv-fms" className="form-control-static">FMS URL:</label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="livecodingtv-fms" className="form-control"/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="livecodingtv-key" className="form-control-static i18n-stream-key"></label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="livecodingtv-key" className="form-control"/>
-            </div>
-          </div>
-          <div style={{marginTop: "1em"}} className="row">
-            <div className="col-sm-12 text-right">
-            <a href="https://www.livecoding.tv/" target="_blank" className="selectable">https://www.livecoding.tv/</a>
-            </div>
-          </div>
-        </div>;
-}
-
-function NiconicoContents() {
-    return         <div style={{display: "none"}} id="niconico-option" className="col-sm-8">
-          <div className="row">
-            <div className="col-sm-push-3 col-sm-9">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" id="niconico-enabled"/><span className="i18n-enable"></span>
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="niconico-fms" className="form-control-static">FMS URL:</label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="niconico-fms" className="form-control"/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="niconico-key" className="form-control-static i18n-stream-key"></label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="niconico-key" className="form-control"/>
-            </div>
-          </div>
-          <div style={{marginTop: "1em"}} className="row">
-            <div className="col-sm-12 text-right">
-            <a href="http://live.nicovideo.jp/" target="_blank" className="selectable">http://live.nicovideo.jp/</a>
-            </div>
-          </div>
-        </div>;
-}
-
-function OtherContents() {
-    return         <div style={{display: "none"}} id="other-option" className="col-sm-8">
-          <div className="row">
-            <div className="col-sm-push-3 col-sm-9">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" id="other-enabled"/><span className="i18n-enable"></span>
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="other-fms" className="form-control-static">FMS URL:</label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="other-fms" className="form-control"/>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-3 text-right">
-              <label for="other-key" className="form-control-static i18n-stream-key"></label>
-            </div>
-            <div className="col-sm-9">
-              <input style={{width: "100%"}} type="text" id="other-key" className="form-control"/>
-            </div>
-          </div>
-        </div>;
-}
-
-function Contents() {
-        return <div>
-        <TwitchContents/>
-        <PeerCastStationContents/>
-        <LivecodingTvContents/>
-        <NiconicoContents/>
-        <OtherContents/>
-</div>;
+        </div>
+    </div>;
 }
